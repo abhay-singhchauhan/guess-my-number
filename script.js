715e1618d5210a354b64e554704431eedc5edbb7
abhay <itsyourabhay@gmail.com>
2022-09-20 18:45:09 +0530

    First Commit Of this repo

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let score = 20;
+let highScore = 0;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = `
+        😲 No Number`;
+  }
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🤩 You're Correct.`
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem'
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+  else if (guess !== secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = guess < secretNumber ? `😞 Your guess is too Low` : `😞 Your guess is too High`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+    else {
+      document.querySelector('.message').textContent = `😞 You Loose the Game`
+      document.querySelector('.score').textContent = 0;
+    }
+  };
+
+});
+
+document.querySelector('.again').addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.message').textContent = `Start guessing...`
+})
